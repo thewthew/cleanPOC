44f71fa6ea441961d7813c26a468e9a2517b9b1c
Matthew Usdin <matthew.usdin.ext@orangebank.com>
2019-01-25 10:09:15 +0100

    Initial Commit

---
+++ Clean-tableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clean-tableView
+//
+//  Created by Matthew Usdin on 25/01/2019.
+//  Copyright © 2019 Matthew Usdin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
